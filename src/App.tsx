d82abba2d109d76c586da79b2c2169a41027a9fe
AchillesKal <achilleas90gr@gmail.com>
2020-08-26 17:16:19 +0300

    Remove useless comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-// import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// function App() {
-//   return <div className="App"></div>;
-// }
-
-// export default App;
-
 import React, { Component } from "react";
 import { render } from "react-dom";
 
